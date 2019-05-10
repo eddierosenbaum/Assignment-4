15ac2dc93ddbafcd7fcd452d17f40706e8c6acf4
eddierosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-05-10 08:18:26 -0400

    New

---
+++ js/resources.js
@@ -0,0 +1,18 @@
+var MdMClinics = [
+    {
+      "name": "Toshkivka",
+      "Mobile Clinics": "Toshkivka",
+      "lng": 48.788572,
+      "lat": 38.5137507
+    },
+    {
+      "name": "Stepove",
+      "Moblie Clinics": "Stepove",
+      "lng": 48.46389,
+      "lat": 39.34227
+    },
+    {
+      "name": "Stanytsia Luhanska",
+      "Mobile Clinics": "Stanytsia Luhanska",
+      "lng": 48.6754626,
+      "lat": 39.4681136
