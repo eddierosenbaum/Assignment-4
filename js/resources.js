ca94d7df845cf45c97b021a39966b372756b4d38
eddierosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-05-12 17:31:06 -0400

    Update resources.js

--- js/resources.js
+++ js/resources.js
@@ -1,3 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWRkaWVyb3NlbmJhdW0iLCJhIjoiY2p1a2hjYnN1MXZ5bDQzcGcyd3M2djkyNCJ9._8wwgcVBrEeXxMoTgrXRGg';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/eddierosenbaum/cjuklbw401zjh1fqueauiyv2y',
+  center: [32.288818,48.814099],
+  zoom: 4.0
+});
 var clinics= [
   {
     lng: 38.5137507,
@@ -50,3 +57,13 @@ var clinics= [
     name: 'Komyshne'
   },
 ];
+console.log(clinics)
+clinics.forEach(function(clinic) {
+
+
+  new mapboxgl.Marker()
+    .setLngLat([clinic.lng, clinic.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 40 })
+      .setText(`MdM Clinic: ${clinic.name}`))
+    .addTo(map);
+})
