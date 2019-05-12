d4dbb8823d6936a6a0d40e2f8b9950e20ccd9981
eddierosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-05-12 17:26:54 -0400

    Added more clinics

--- js/resources.js
+++ js/resources.js
@@ -1,19 +1,52 @@
-var MdMClinics = [
-    {
-      "name": "Toshkivka",
-      "Mobile Clinics": "Toshkivka",
-      "lng": 48.788572,
-      "lat": 38.5137507
-    },
-    {
-      "name": "Stepove",
-      "Moblie Clinics": "Stepove",
-      "lng": 48.46389,
-      "lat": 39.34227
-    },
-    {
-      
-      "name": "Stanytsia Luhanska",
-      "Mobile Clinics": "Stanytsia Luhanska",
-      "lng": 48.6754626,
-      "lat": 39.4681136
+var clinics= [
+  {
+    lng: 38.5137507,
+    lat: 48.788572,
+    name: 'Toshkivka'
+  },
+  {
+    lng: 39.65141,
+    lat: 48.7699,
+    name: 'Stepove'
+  },
+  {
+    lng: 38.5561875,
+    lat: 48.7221869,
+    name: 'Orikhove'
+  },
+  {
+    lng: 38.4287579,
+    lat: 48.7523564,
+    name: 'Novoivanivka'
+  },
+  {
+    lng: 38.2952848,
+    lat: 48.6928385,
+    name: 'Zolote'
+  },
+  {
+    lng: 38.2905473,
+    lat: 48.6450918,
+    name: 'Druzhba'
+  },
+  {
+    lng: 39.0904825,
+    lat: 48.7685625,
+    name: 'Heivka'
+  },
+  {
+    lng: 38.4598932,
+    lat: 48.7517199,
+    name: 'Hirske'
+  },
+  {
+    lng: 39.6234141,
+    lat: 48.7193748,
+    name: 'Komyshne'
+  },
+  {
+    lng: 39.6234141,
+    lat: 48.7193748,
+    name: 'Komyshne'
+  },
+];
